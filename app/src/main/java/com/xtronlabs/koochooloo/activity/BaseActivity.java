0a74a85cc33166139351245ed97f5b7edcacea42
sonyraj <sonyraj.s@gmail.com>
2016-07-15 15:49:11 +0530

    Initial commit with 4 layouts

---
+++ app/src/main/java/com/xtronlabs/koochooloo/activity/BaseActivity.java
@@ -0,0 +1,7 @@
+package com.xtronlabs.koochooloo.activity;
+
+/**
+ * Created by Xtron005 on 14-07-2016.
+ */
+public class BaseActivity {
+}
---
+++ app/src/main/java/com/xtronlabs/koochooloo/fragment/BaseFragment.java
@@ -0,0 +1,7 @@
+package com.xtronlabs.koochooloo.fragment;
+
+/**
+ * Created by Xtron005 on 14-07-2016.
+ */
+public class BaseFragment {
+}
